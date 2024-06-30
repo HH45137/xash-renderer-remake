5af87d750017b5146c564fe5df18890853c6da08
Carl-Y <52552476+HH45137@users.noreply.github.com>
2024-06-30 20:29:38 +0800

    setupRenderPass

--- src/ref_vk.cpp
+++ src/ref_vk.cpp
@@ -75,6 +75,7 @@ namespace REF_VK {
             VmaAllocation allocation;
             VkImageView imageView;
         } depthStencil;
+        VkRenderPass renderPass{};
 
         VmaAllocator vmaAllocator;
 
@@ -186,6 +187,8 @@ namespace REF_VK {
             createSynchronizationPrimitives();
 
             setupDepthStencil();
+
+            setupRenderPass();
 
             // Create vertex buffer
 //            createVertexBuffer();
@@ -350,6 +353,83 @@ namespace REF_VK {
             VK_CHECK_RESULT(
                     vkCreateImageView(logicDevice, &imageViewCI, nullptr, &depthStencil.imageView));
 
+        }
+
+        void setupRenderPass() {
+            std::array<VkAttachmentDescription, 2> attachments{};
+
+            // Color attachment
+            attachments[0].format = swapChain.colorFormat;
+            attachments[0].samples = VK_SAMPLE_COUNT_1_BIT;
+            attachments[0].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+            attachments[0].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+            attachments[0].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+            attachments[0].stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+            attachments[0].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+            attachments[0].finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+            // Depth attachment
+            attachments[1].format = depthFormat;
+            attachments[1].samples = VK_SAMPLE_COUNT_1_BIT;
+            attachments[1].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+            attachments[1].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+            attachments[1].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+            attachments[1].stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+            attachments[1].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+            attachments[1].finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+
+            VkAttachmentReference colorReference{};
+            colorReference.attachment = 0;
+            colorReference.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+            VkAttachmentReference depthReference{};
+            depthReference.attachment = 1;
+            depthReference.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+
+            VkSubpassDescription subpassDescription{};
+            subpassDescription.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+            subpassDescription.colorAttachmentCount = 1;
+            subpassDescription.pColorAttachments = &colorReference;
+            subpassDescription.pDepthStencilAttachment = &depthReference;
+            subpassDescription.inputAttachmentCount = 0;
+            subpassDescription.pInputAttachments = nullptr;
+            subpassDescription.preserveAttachmentCount = 0;
+            subpassDescription.pPreserveAttachments = nullptr;
+            subpassDescription.pResolveAttachments = nullptr;
+
+            // Subpass dependencies for layout transitions
+            std::array<VkSubpassDependency, 2> dependencies{};
+
+            dependencies[0].srcSubpass = VK_SUBPASS_EXTERNAL;
+            dependencies[0].dstSubpass = 0;
+            dependencies[0].srcStageMask =
+                    VK_PIPELINE_STAGE_2_EARLY_FRAGMENT_TESTS_BIT | VK_PIPELINE_STAGE_2_LATE_FRAGMENT_TESTS_BIT;
+            dependencies[0].dstStageMask =
+                    VK_PIPELINE_STAGE_2_EARLY_FRAGMENT_TESTS_BIT | VK_PIPELINE_STAGE_2_LATE_FRAGMENT_TESTS_BIT;
+            dependencies[0].srcAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+            dependencies[0].dstAccessMask =
+                    VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT;
+            dependencies[0].dependencyFlags = 0;
+
+            dependencies[1].srcSubpass = VK_SUBPASS_EXTERNAL;
+            dependencies[1].dstSubpass = 0;
+            dependencies[1].srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+            dependencies[1].dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+            dependencies[1].srcAccessMask = 0;
+            dependencies[1].dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT | VK_ACCESS_COLOR_ATTACHMENT_READ_BIT;
+            dependencies[1].dependencyFlags = 0;
+
+            VkRenderPassCreateInfo renderPassCI{};
+            renderPassCI.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+            renderPassCI.attachmentCount = static_cast<uint32_t>(attachments.size());
+            renderPassCI.pAttachments = attachments.data();
+            renderPassCI.subpassCount = 1;
+            renderPassCI.pSubpasses = &subpassDescription;
+            renderPassCI.dependencyCount = static_cast<uint32_t>(dependencies.size());
+            renderPassCI.pDependencies = dependencies.data();
+
+            VK_CHECK_RESULT(vkCreateRenderPass(logicDevice, &renderPassCI, nullptr, &renderPass));
+            
         }
 
         void createVertexBuffer() {
