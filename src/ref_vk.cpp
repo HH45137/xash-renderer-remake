87021536d1531fc79a13a784f5526d1c0b22143f
Carl-Y <52552476+HH45137@users.noreply.github.com>
2024-07-01 18:38:04 +0800

    The ref_vk createSynchronizationPrimitives

--- src/ref_vk.cpp
+++ src/ref_vk.cpp
@@ -18,137 +18,173 @@
 
 #endif
 
+#define MAX_CONCURRENT_FRAMES 2
+
+
 namespace REF_VK {
 
     class CRef_Vk : public VulkanAppBase {
     public:
-        void createVertexBuffer() {
+        void createSynchronizationPrimitives();
 
-            // Vertex data
-            std::vector<Vertex> vertexBuffer{
-                    {{1.0f,  1.0f,  0.0f}, {1.0f, 0.0f, 0.0f}},
-                    {{-1.0f, 1.0f,  0.0f}, {0.0f, 1.0f, 0.0f}},
-                    {{0.0f,  -1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}}
-            };
-            uint32_t vertexBufferSize = static_cast<uint32_t>(vertexBuffer.size() * sizeof(Vertex));
+        void createVertexBuffer();
 
-            // Index data
-            std::vector<uint32_t> indexBuffer{0,
-                                              1,
-                                              2};
-            uint32_t indexBufferSize = static_cast<uint32_t>(indexBuffer.size() * sizeof(uint32_t));
-            indices.count = indexBufferSize;
-
-            // Temp staging buffer
-            struct SStagingBuffer {
-                VkBuffer buffer;
-                VmaAllocation allocation;
-            };
-            struct {
-                SStagingBuffer vertices;
-                SStagingBuffer indices;
-            } stagingBuffers{};
-
-            // VMA memory usage auto
-            VmaAllocationCreateInfo allocInfo{};
-            allocInfo.usage = VMA_MEMORY_USAGE_AUTO;
-            allocInfo.flags = VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | VMA_ALLOCATION_CREATE_MAPPED_BIT;
-
-            // Allocator vertex staging buffer memory
-            VkBufferCreateInfo vbCI{};
-            vbCI.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-            vbCI.size = vertexBufferSize;
-            vbCI.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
-            vbCI.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
-            VmaAllocationInfo stagingVertexBufferAllocInfo{};
-            bool result = VK_CHECK_RESULT(
-                    vmaCreateBuffer(vmaAllocator, &vbCI, &allocInfo, &stagingBuffers.vertices.buffer,
-                                    &stagingBuffers.vertices.allocation,
-                                    &stagingVertexBufferAllocInfo));
-            // Map and copy
-            void *data = stagingVertexBufferAllocInfo.pMappedData;
-//            vmaMapMemory(vmaAllocator, stagingBuffers.vertices.allocation, &data);
-            memcpy(data, vertexBuffer.data(), vertexBufferSize);
-//            vmaUnmapMemory(vmaAllocator, stagingBuffers.vertices.allocation);
-            // Allocator vertex buffer memory
-            vbCI.usage = VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
-            vbCI.flags = 0;
-            vmaCreateBuffer(vmaAllocator, &vbCI, &allocInfo, &vertices.buffer, &vertices.allocation, nullptr);
-
-            // Allocator index staging buffer memory
-            VkBufferCreateInfo ibCI{};
-            ibCI.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-            ibCI.size = indexBufferSize;
-            ibCI.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
-            ibCI.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
-            VmaAllocationInfo stagingIndexBufferAllocInfo{};
-            result = VK_CHECK_RESULT(
-                    vmaCreateBuffer(vmaAllocator, &ibCI, &allocInfo, &stagingBuffers.indices.buffer,
-                                    &stagingBuffers.indices.allocation,
-                                    &stagingIndexBufferAllocInfo));
-            // Map and copy
-            data = stagingIndexBufferAllocInfo.pMappedData;
-//            vmaMapMemory(vmaAllocator, stagingBuffers.indices.allocation, &data);
-            memcpy(data, indexBuffer.data(), indexBufferSize);
-//            vmaUnmapMemory(vmaAllocator, stagingBuffers.indices.allocation);
-            // Allocator index buffer memory
-            ibCI.usage = VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT;
-            ibCI.flags = 0;
-            vmaCreateBuffer(vmaAllocator, &ibCI, &allocInfo, &indices.buffer, &indices.allocation, nullptr);
-
-
-            //Copy command buffer submit
-            VkCommandBuffer copyCmdBuf;
-
-            VkCommandBufferAllocateInfo cmdBuffAllocateInfo{};
-            cmdBuffAllocateInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-            cmdBuffAllocateInfo.commandPool = cmdPool;
-            cmdBuffAllocateInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-            cmdBuffAllocateInfo.commandBufferCount = 1;
-            VK_CHECK_RESULT(vkAllocateCommandBuffers(logicDevice, &cmdBuffAllocateInfo, &copyCmdBuf));
-
-            // Start command
-            VkCommandBufferBeginInfo cmdBuffBeginInfo{};
-            cmdBuffBeginInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-            VK_CHECK_RESULT(vkBeginCommandBuffer(copyCmdBuf, &cmdBuffBeginInfo));
-            {
-                VkBufferCopy copyRegion{};
-                // Vertex buffer copy
-                copyRegion.size = vertexBufferSize;
-                vkCmdCopyBuffer(copyCmdBuf, stagingBuffers.vertices.buffer, vertices.buffer, 1, &copyRegion);
-                // Index buffer copy
-                copyRegion.size = indexBufferSize;
-                vkCmdCopyBuffer(copyCmdBuf, stagingBuffers.indices.buffer, indices.buffer, 1, &copyRegion);
-            }
-            // Stop command
-            VK_CHECK_RESULT(vkEndCommandBuffer(copyCmdBuf));
-
-            // Create fence for wait command buffer executed
-            VkFenceCreateInfo fenceCI{};
-            fenceCI.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
-            fenceCI.flags = 0;
-            VkFence fence;
-            VK_CHECK_RESULT(vkCreateFence(logicDevice, &fenceCI, nullptr, &fence));
-
-            // Submit to queue
-            VK_CHECK_RESULT(vkQueueSubmit(queue, 1, &submitInfo, fence));
-            // Wait...
-            VK_CHECK_RESULT(vkWaitForFences(logicDevice, 1, &fence, VK_TRUE, DEFAULT_FENCE_TIMEOUT));
-
-            vkDestroyFence(logicDevice, fence, nullptr);
-            vkFreeCommandBuffers(logicDevice, cmdPool, 1, &copyCmdBuf);
-
-            vmaDestroyBuffer(vmaAllocator, stagingBuffers.vertices.buffer, stagingBuffers.vertices.allocation);
-            vmaFreeMemory(vmaAllocator, stagingBuffers.vertices.allocation);
-            vmaDestroyBuffer(vmaAllocator, stagingBuffers.indices.buffer, stagingBuffers.indices.allocation);
-            vmaFreeMemory(vmaAllocator, stagingBuffers.indices.allocation);
-
-            return;
-        }
+        bool prepare();
 
     private:
-
+        std::array<VkSemaphore, MAX_CONCURRENT_FRAMES> presentCompleteSemaphores{};
+        std::array<VkSemaphore, MAX_CONCURRENT_FRAMES> renderCompleteSemaphores{};
+        std::array<VkFence, MAX_CONCURRENT_FRAMES> waitFences{};
     };
+
+    void CRef_Vk::createSynchronizationPrimitives() {
+        VkSemaphoreCreateInfo semaphoreCI{};
+        semaphoreCI.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
+
+        VkFenceCreateInfo fenceCI{};
+        fenceCI.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+        fenceCI.flags = VK_FENCE_CREATE_SIGNALED_BIT;
+
+        for (int i = 0; i < MAX_CONCURRENT_FRAMES; ++i) {
+            VK_CHECK_RESULT(vkCreateSemaphore(logicDevice, &semaphoreCI, nullptr, &presentCompleteSemaphores[i]));
+            VK_CHECK_RESULT(vkCreateSemaphore(logicDevice, &semaphoreCI, nullptr, &renderCompleteSemaphores[i]));
+
+            VK_CHECK_RESULT(vkCreateFence(logicDevice, &fenceCI, nullptr, &waitFences[i]));
+        }
+
+        return;
+    }
+
+    void CRef_Vk::createVertexBuffer() {
+
+        // Vertex data
+        std::vector<Vertex> vertexBuffer{
+                {{1.0f,  1.0f,  0.0f}, {1.0f, 0.0f, 0.0f}},
+                {{-1.0f, 1.0f,  0.0f}, {0.0f, 1.0f, 0.0f}},
+                {{0.0f,  -1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}}
+        };
+        uint32_t vertexBufferSize = static_cast<uint32_t>(vertexBuffer.size() * sizeof(Vertex));
+
+        // Index data
+        std::vector<uint32_t> indexBuffer{0,
+                                          1,
+                                          2};
+        uint32_t indexBufferSize = static_cast<uint32_t>(indexBuffer.size() * sizeof(uint32_t));
+        indices.count = indexBufferSize;
+
+        // Temp staging buffer
+        struct SStagingBuffer {
+            VkBuffer buffer;
+            VmaAllocation allocation;
+        };
+        struct {
+            SStagingBuffer vertices;
+            SStagingBuffer indices;
+        } stagingBuffers{};
+
+        // VMA memory usage auto
+        VmaAllocationCreateInfo allocInfo{};
+        allocInfo.usage = VMA_MEMORY_USAGE_AUTO;
+        allocInfo.flags = VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | VMA_ALLOCATION_CREATE_MAPPED_BIT;
+
+        // Allocator vertex staging buffer memory
+        VkBufferCreateInfo vbCI{};
+        vbCI.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+        vbCI.size = vertexBufferSize;
+        vbCI.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+        vbCI.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+        VmaAllocationInfo stagingVertexBufferAllocInfo{};
+        bool result = VK_CHECK_RESULT(
+                vmaCreateBuffer(vmaAllocator, &vbCI, &allocInfo, &stagingBuffers.vertices.buffer,
+                                &stagingBuffers.vertices.allocation,
+                                &stagingVertexBufferAllocInfo));
+        // Map and copy
+        void *data = stagingVertexBufferAllocInfo.pMappedData;
+//            vmaMapMemory(vmaAllocator, stagingBuffers.vertices.allocation, &data);
+        memcpy(data, vertexBuffer.data(), vertexBufferSize);
+//            vmaUnmapMemory(vmaAllocator, stagingBuffers.vertices.allocation);
+        // Allocator vertex buffer memory
+        vbCI.usage = VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+        vbCI.flags = 0;
+        vmaCreateBuffer(vmaAllocator, &vbCI, &allocInfo, &vertices.buffer, &vertices.allocation, nullptr);
+
+        // Allocator index staging buffer memory
+        VkBufferCreateInfo ibCI{};
+        ibCI.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+        ibCI.size = indexBufferSize;
+        ibCI.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+        ibCI.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+        VmaAllocationInfo stagingIndexBufferAllocInfo{};
+        result = VK_CHECK_RESULT(
+                vmaCreateBuffer(vmaAllocator, &ibCI, &allocInfo, &stagingBuffers.indices.buffer,
+                                &stagingBuffers.indices.allocation,
+                                &stagingIndexBufferAllocInfo));
+        // Map and copy
+        data = stagingIndexBufferAllocInfo.pMappedData;
+//            vmaMapMemory(vmaAllocator, stagingBuffers.indices.allocation, &data);
+        memcpy(data, indexBuffer.data(), indexBufferSize);
+//            vmaUnmapMemory(vmaAllocator, stagingBuffers.indices.allocation);
+        // Allocator index buffer memory
+        ibCI.usage = VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT;
+        ibCI.flags = 0;
+        vmaCreateBuffer(vmaAllocator, &ibCI, &allocInfo, &indices.buffer, &indices.allocation, nullptr);
+
+
+        //Copy command buffer submit
+        VkCommandBuffer copyCmdBuf;
+
+        VkCommandBufferAllocateInfo cmdBuffAllocateInfo{};
+        cmdBuffAllocateInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        cmdBuffAllocateInfo.commandPool = cmdPool;
+        cmdBuffAllocateInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        cmdBuffAllocateInfo.commandBufferCount = 1;
+        VK_CHECK_RESULT(vkAllocateCommandBuffers(logicDevice, &cmdBuffAllocateInfo, &copyCmdBuf));
+
+        // Start command
+        VkCommandBufferBeginInfo cmdBuffBeginInfo{};
+        cmdBuffBeginInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+        VK_CHECK_RESULT(vkBeginCommandBuffer(copyCmdBuf, &cmdBuffBeginInfo));
+        {
+            VkBufferCopy copyRegion{};
+            // Vertex buffer copy
+            copyRegion.size = vertexBufferSize;
+            vkCmdCopyBuffer(copyCmdBuf, stagingBuffers.vertices.buffer, vertices.buffer, 1, &copyRegion);
+            // Index buffer copy
+            copyRegion.size = indexBufferSize;
+            vkCmdCopyBuffer(copyCmdBuf, stagingBuffers.indices.buffer, indices.buffer, 1, &copyRegion);
+        }
+        // Stop command
+        VK_CHECK_RESULT(vkEndCommandBuffer(copyCmdBuf));
+
+        // Create fence for wait command buffer executed
+        VkFenceCreateInfo fenceCI{};
+        fenceCI.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+        fenceCI.flags = 0;
+        VkFence fence;
+        VK_CHECK_RESULT(vkCreateFence(logicDevice, &fenceCI, nullptr, &fence));
+
+        // Submit to queue
+        VK_CHECK_RESULT(vkQueueSubmit(queue, 1, &submitInfo, fence));
+        // Wait...
+        VK_CHECK_RESULT(vkWaitForFences(logicDevice, 1, &fence, VK_TRUE, DEFAULT_FENCE_TIMEOUT));
+
+        vkDestroyFence(logicDevice, fence, nullptr);
+        vkFreeCommandBuffers(logicDevice, cmdPool, 1, &copyCmdBuf);
+
+        vmaDestroyBuffer(vmaAllocator, stagingBuffers.vertices.buffer, stagingBuffers.vertices.allocation);
+        vmaFreeMemory(vmaAllocator, stagingBuffers.vertices.allocation);
+        vmaDestroyBuffer(vmaAllocator, stagingBuffers.indices.buffer, stagingBuffers.indices.allocation);
+        vmaFreeMemory(vmaAllocator, stagingBuffers.indices.allocation);
+
+        return;
+    }
+
+    bool CRef_Vk::prepare() {
+        VulkanAppBase::prepare();
+        createSynchronizationPrimitives();
+
+        return true;
+    }
 
     CRef_Vk ref_vk_obj{};
 }
